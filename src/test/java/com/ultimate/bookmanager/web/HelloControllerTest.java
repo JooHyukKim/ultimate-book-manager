ef5f9858b4df8164568787271ad7e47ba00ea8e9
VincePlantItAi <61615301+JooHyukKim@users.noreply.github.com>
2021-08-30 23:22:30 +0900

    FEAT : responseDto created& Test

--- src/test/java/com/ultimate/bookmanager/web/HelloControllerTest.java
+++ src/test/java/com/ultimate/bookmanager/web/HelloControllerTest.java
@@ -7,9 +7,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(HellController.class)
@@ -19,12 +21,29 @@ public class HelloControllerTest {
     private MockMvc mvc;
 
     @Test
-    public void hello() throws Exception {
+    public void helloTest() throws Exception {
         String hello = "hello";
 
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("hello"));
+                .andExpect(content().string("hello")).andReturn();
+    }
+
+    @Test
+    public void helloResponseDtoTest() throws Exception {
+        String name = "john";
+        int amount = 10;
+
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount))
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)))
+                .andReturn();
+
     }
 
 }
